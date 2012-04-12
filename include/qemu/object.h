88d8f50aa8dbaa84c9062914c81fef7029dfe01f
Paolo Bonzini <pbonzini@redhat.com>
2012-04-12 18:03:01 +0200

    qom: documentation addition
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/qemu/object.h
+++ include/qemu/object.h
@@ -555,6 +555,12 @@ ObjectClass *object_class_dynamic_cast(ObjectClass *klass,
  */
 const char *object_class_get_name(ObjectClass *klass);
 
+/**
+ * object_class_by_name:
+ * @typename: The QOM typename to obtain the class for.
+ *
+ * Returns: The class for @typename.
+ */
 ObjectClass *object_class_by_name(const char *typename);
 
 void object_class_foreach(void (*fn)(ObjectClass *klass, void *opaque),
