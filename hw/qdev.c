c1f9c3e6b874670731c3d8195daba9b61019d8c7
Paolo Bonzini <pbonzini@redhat.com>
2012-04-12 15:49:27 +0200

    qom: add get_id
    Some classes may present objects differently in errors, for example if they
    are not part of the composition tree or if they are not assigned an id by
    the user.  Let them do this with a get_id method on Object, and use the
    method consistently where a %(device) appears in the error.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/qdev.c
+++ hw/qdev.c
@@ -235,7 +235,7 @@ void qdev_init_nofail(DeviceState *dev)
 {
     if (qdev_init(dev) < 0) {
         error_report("Initialization of device %s failed",
-                     object_get_typename(OBJECT(dev)));
+                     object_get_id(OBJECT(dev)));
         exit(1);
     }
 }
@@ -661,6 +661,13 @@ static void device_finalize(Object *obj)
     QTAILQ_REMOVE(&dev->parent_bus->children, dev, sibling);
 }
 
+static const char *qdev_get_id(Object *obj)
+{
+    DeviceState *dev = DEVICE(obj);
+
+    return dev->id?:object_get_typename(obj);
+}
+
 static void device_class_base_init(ObjectClass *class, void *data)
 {
     DeviceClass *klass = DEVICE_CLASS(class);
@@ -691,6 +698,11 @@ Object *qdev_get_machine(void)
     return dev;
 }
 
+static void device_class_init(ObjectClass *class, void *data)
+{
+    class->get_id = qdev_get_id;
+}
+
 static TypeInfo device_type_info = {
     .name = TYPE_DEVICE,
     .parent = TYPE_OBJECT,
@@ -698,6 +710,7 @@ static TypeInfo device_type_info = {
     .instance_init = device_initfn,
     .instance_finalize = device_finalize,
     .class_base_init = device_class_base_init,
+    .class_init = device_class_init,
     .abstract = true,
     .class_size = sizeof(DeviceClass),
 };
