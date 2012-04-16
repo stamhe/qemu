db471a0b61975d5ee5e365ae758a4738dfacdb40
Igor Mammedov <niallain@gmail.com>
2012-04-16 18:05:27 +0200

    target-i386: move cpu halted decision into x86_cpu_reset
    MP initialization protocol differs between cpu families, and for P6 and
    onward models it is up to CPU to decide if it will be BSP using this
    protocol, so try to model this. However there is no point in implementing
    MP initialization protocol in qemu. Thus first CPU is always marked as BSP.
    
    This patch:
     - moves decision to designate BSP from board into cpu, making cpu
    self-sufficient in this regard. Later it will allow to cleanup hw/pc.c
    and remove cpu_reset and wrappers from there.
     - stores flag that CPU is BSP in IA32_APIC_BASE to model behavior
    described in Inted SDM vol 3a part 1 chapter 8.4.1
     - uses MSR_IA32_APICBASE_BSP flag in apic_base for checking if cpu is BSP
    
    patch is based on Jan Kiszka's proposal:
        http://thread.gmane.org/gmane.comp.emulators.qemu/100806
    
    v2:
      - fix build for i386-linux-user
          spotted-by: Peter Maydell <peter.maydell@linaro.org>
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>

--- hw/apic.h
+++ hw/apic.h
@@ -20,9 +20,9 @@ void apic_init_reset(DeviceState *s);
 void apic_sipi(DeviceState *s);
 void apic_handle_tpr_access_report(DeviceState *d, target_ulong ip,
                                    TPRAccess access);
+void apic_designate_bsp(DeviceState *d);
 
 /* pc.c */
-int cpu_is_bsp(CPUX86State *env);
 DeviceState *cpu_get_current_apic(void);
 
 #endif
