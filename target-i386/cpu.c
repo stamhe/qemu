70586df4e034e442a463eb9ccd7ee99bf9757254
Igor Mammedov <imammedo@redhat.com>
2012-08-01 01:08:38 +0200

    target-i386: cpu_x86_register(): report error from property setter
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>

--- target-i386/cpu.c
+++ target-i386/cpu.c
@@ -1186,6 +1186,7 @@ int cpu_x86_register(X86CPU *cpu, const char *cpu_model)
 
 out:
     if (error_is_set(&error)) {
+        fprintf(stderr, "%s\n", error_get_pretty(error));
         error_free(error);
         return -1;
     }
