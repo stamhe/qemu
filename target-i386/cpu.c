88e1121cdec3dd858106429699c1fe944db617b1
Igor Mammedov <imammedo@redhat.com>
2012-08-08 13:54:27 +0200

    target-i386: convert 'hv_vapic' to static property
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    ---
    v3:
     - cpu_x86_properties changed to x86_cpu_properties,
       upstream rebase on top of it.
    v2:
     - rebase on top of hv_vapic in X86CPU

--- target-i386/cpu.c
+++ target-i386/cpu.c
@@ -1765,7 +1765,7 @@ static void cpu_x86_parse_featurestr(X86CPU *cpu, char *features, Error **errp)
         } else if (!strcmp(featurestr, "hv_relaxed")) {
             object_property_parse(OBJECT(cpu), "on", "hv-relaxed", errp);
         } else if (!strcmp(featurestr, "hv_vapic")) {
-            cpu->hyperv_vapic = true;
+            object_property_parse(OBJECT(cpu), "on", "hv-vapic", errp);
         } else {
             error_setg(errp, "feature string `%s' not in format (+feature|"
                        "-feature|feature=xyz)", featurestr);
@@ -2720,6 +2720,7 @@ static Property x86_cpu_properties[] = {
     DEFINE_PROP_BOOL("pmu", X86CPU, enable_pmu, false),
     { .name  = "hv-spinlocks", .info  = &qdev_prop_spinlocks },
     DEFINE_PROP_BOOL("hv-relaxed", X86CPU, hyperv_relaxed_timing, false),
+    DEFINE_PROP_BOOL("hv-vapic", X86CPU, hyperv_vapic, false),
     DEFINE_PROP_END_OF_LIST()
 };
 
