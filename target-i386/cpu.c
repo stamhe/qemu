f0ee64bfa44d4568bcc3dc2e7c6f72b18c149988
Igor Mammedov <imammedo@redhat.com>
2012-10-12 16:37:12 +0200

    target-i386: convert "xlevel" to static property
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>

--- target-i386/cpu.c
+++ target-i386/cpu.c
@@ -454,6 +454,7 @@ static Property cpu_x86_properties[] = {
     FEAT("f-adx", env.cpuid_7_0_ebx_features, 19, false),
     FEAT("f-smap", env.cpuid_7_0_ebx_features, 20, false),
     DEFINE_PROP_VENDOR("vendor", X86CPU, env.cpuid_vendor1),
+    DEFINE_PROP_UINT32("xlevel", X86CPU, env.cpuid_xlevel, 0),
     DEFINE_PROP_END_OF_LIST(),
  };
 
@@ -1300,22 +1301,6 @@ static void x86_cpuid_set_level(Object *obj, Visitor *v, void *opaque,
     X86CPU *cpu = X86_CPU(obj);
 
     visit_type_uint32(v, &cpu->env.cpuid_level, name, errp);
-}
-
-static void x86_cpuid_get_xlevel(Object *obj, Visitor *v, void *opaque,
-                                 const char *name, Error **errp)
-{
-    X86CPU *cpu = X86_CPU(obj);
-
-    visit_type_uint32(v, &cpu->env.cpuid_xlevel, name, errp);
-}
-
-static void x86_cpuid_set_xlevel(Object *obj, Visitor *v, void *opaque,
-                                 const char *name, Error **errp)
-{
-    X86CPU *cpu = X86_CPU(obj);
-
-    visit_type_uint32(v, &cpu->env.cpuid_xlevel, name, errp);
 }
 
 static char *x86_cpuid_get_model_id(Object *obj, Error **errp)
@@ -2271,9 +2256,6 @@ static void x86_cpu_initfn(Object *obj)
     object_property_add(obj, "level", "int",
                         x86_cpuid_get_level,
                         x86_cpuid_set_level, NULL, NULL, NULL);
-    object_property_add(obj, "xlevel", "int",
-                        x86_cpuid_get_xlevel,
-                        x86_cpuid_set_xlevel, NULL, NULL, NULL);
     object_property_add_str(obj, "model-id",
                             x86_cpuid_get_model_id,
                             x86_cpuid_set_model_id, NULL);
