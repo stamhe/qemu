0a3fdbc037d3654d735f73ff634118c111b89ead
Eduardo Habkost <ehabkost@redhat.com>
2012-10-16 16:08:34 -0300

    qdev: separate core from the code used only by qemu-system-*
    This change should help on two things:
     - Allowing DeviceState to be used by *-user;
     - Writing qdev unit tests without pulling too many dependencies.
    
    Note that there are two parts that depend on code compiled only on
    qemu-system-*, but are still inside qdev.c:
     - vmstate handling
     - reset function registration.
    
    Those two parts will be handled later, by moving that code to
    qdev-system.c but keeping weak symbols for *-user.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    [imammedo: keep qdev_get_child_bus() in hw/qdev.c]
    [imammedo: put qdev_set_nic_properties() in hw/qdev-properties-system.c]
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    [ehabkost: updated the qdev_init_gpio_in() code on qdev-system.c to current
     version]
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

---
+++ hw/qdev-system.c
@@ -0,0 +1,68 @@
+#include "qdev.h"
+
+void qdev_init_gpio_in(DeviceState *dev, qemu_irq_handler handler, int n)
+{
+    dev->gpio_in = qemu_extend_irqs(dev->gpio_in, dev->num_gpio_in, handler,
+                                        dev, n);
+    dev->num_gpio_in += n;
+}
+
+void qdev_init_gpio_out(DeviceState *dev, qemu_irq *pins, int n)
+{
+    assert(dev->num_gpio_out == 0);
+    dev->num_gpio_out = n;
+    dev->gpio_out = pins;
+}
+
+qemu_irq qdev_get_gpio_in(DeviceState *dev, int n)
+{
+    assert(n >= 0 && n < dev->num_gpio_in);
+    return dev->gpio_in[n];
+}
+
+void qdev_connect_gpio_out(DeviceState * dev, int n, qemu_irq pin)
+{
+    assert(n >= 0 && n < dev->num_gpio_out);
+    dev->gpio_out[n] = pin;
+}
+
+/* Create a new device.  This only initializes the device state structure
+   and allows properties to be set.  qdev_init should be called to
+   initialize the actual device emulation.  */
+DeviceState *qdev_create(BusState *bus, const char *name)
+{
+    DeviceState *dev;
+
+    dev = qdev_try_create(bus, name);
+    if (!dev) {
+        if (bus) {
+            hw_error("Unknown device '%s' for bus '%s'\n", name,
+                     object_get_typename(OBJECT(bus)));
+        } else {
+            hw_error("Unknown device '%s' for default sysbus\n", name);
+        }
+    }
+
+    return dev;
+}
+
+DeviceState *qdev_try_create(BusState *bus, const char *type)
+{
+    DeviceState *dev;
+
+    if (object_class_by_name(type) == NULL) {
+        return NULL;
+    }
+    dev = DEVICE(object_new(type));
+    if (!dev) {
+        return NULL;
+    }
+
+    if (!bus) {
+        bus = sysbus_get_default();
+    }
+
+    qdev_set_parent_bus(dev, bus);
+
+    return dev;
+}
