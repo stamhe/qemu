8810cc6f8f1845d35db816021a1e46d4934607a4
Igor Mammedov <imammedo@redhat.com>
2013-07-22 12:38:49 +0200

    dimm: add busy slot check and slot auto-allocation
    - if slot property is not specified on -device/device_add command,
    treat default value as request for assigning DimmDevice to
    the first free slot.
    
    - if slot is provided with -device/device_add command, attempt to
    use it or fail command if it's already occupied.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    ---
    v2:
      s/dc/dbc/

--- include/hw/mem-hotplug/dimm.h
+++ include/hw/mem-hotplug/dimm.h
@@ -36,6 +36,7 @@
  * @size: amount of memory mapped at @start.
  * @node: numa node to which @DimmDevice is attached.
  * @slot: slot number into which @DimmDevice is plugged in.
+ * Default value: -1, means that slot is auto-allocated.
  */
 typedef struct DimmDevice {
     DeviceState parent_obj;
@@ -72,11 +73,15 @@ typedef struct DimmBus {
 /**
  * DimmBusClass:
  * @parent_class: opaque parent class container
+ * @get_free_slot: returns a not occupied slot number. If @hint is provided,
+ * it tries to return slot specified by @hint if it's not busy or returns
+ * error in @errp.
  * @register_memory: map @DimmDevice into hot-plugable address space
  */
 typedef struct DimmBusClass {
     BusClass parent_class;
 
+    int (*get_free_slot)(DimmBus *bus, const int *hint, Error **errp);
     void (*register_memory)(DimmBus *bus, DimmDevice *dimm, Error **errp);
 } DimmBusClass;
 
