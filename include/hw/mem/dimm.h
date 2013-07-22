997a33a192a29a52684a8eb985d0064d01c8b6a3
Vasilis Liaskovitis <vasilis.liaskovitis@profitbricks.com>
2013-07-22 10:50:48 +0200

    dimm: implement dimm device abstraction
    Each hotplug-able memory slot is a DimmDevice. All DimmDevices are
    attached to a new bus called DimmBus.
    
    A hot-add operation for a DIMM:
    - creates a new DimmDevice and attaches it to the DimmBus
    
    Hotplug operations are done through normal device_add commands.
    For migration case, all hotplugged DIMMs on source should be specified
    on target's command line using '-device' option with properties set to
    the same values as on target.
    
    To simplify review, patch introduces only DimmDevice and DimmBus basic
    QOM skeleton that will be extended by following patches to implement
    actual memory hotplug and related functions.
    
    Signed-off-by: Vasilis Liaskovitis <vasilis.liaskovitis@profitbricks.com>
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    ---
    v3:
      pc: compile memhotplug on i386 target too
    v2:
      fix typo s/DimmBus/DimmDevice/ in doc comment
      s/klass/oc/;s/*/parent_obj/;a/gtk-doc markup/
    
    dimm impl

---
+++ include/hw/mem/dimm.h
@@ -0,0 +1,71 @@
+/*
+ * DIMM device
+ *
+ * Copyright ProfitBricks GmbH 2012
+ * Copyright (C) 2013 Red Hat Inc
+ *
+ * Authors:
+ *  Vasilis Liaskovitis <vasilis.liaskovitis@profitbricks.com>
+ *  Igor Mammedov <imammedo@redhat.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+
+#ifndef QEMU_DIMM_H
+#define QEMU_DIMM_H
+
+#include "exec/memory.h"
+#include "sysemu/hostmem.h"
+#include "hw/qdev.h"
+
+#define DEFAULT_DIMMSIZE (1024*1024*1024)
+
+#define TYPE_DIMM "dimm"
+#define DIMM(obj) \
+    OBJECT_CHECK(DimmDevice, (obj), TYPE_DIMM)
+#define DIMM_CLASS(oc) \
+    OBJECT_CLASS_CHECK(DimmDeviceClass, (oc), TYPE_DIMM)
+#define DIMM_GET_CLASS(obj) \
+    OBJECT_GET_CLASS(DimmDeviceClass, (obj), TYPE_DIMM)
+
+/**
+ * DimmDevice:
+ * @parent_obj: opaque parent object container
+ * @start: starting guest physical address, where @DimmDevice is mapped.
+ *         Default value: 0, means that address is auto-allocated.
+ * @node: numa node to which @DimmDevice is attached.
+ * @slot: slot number into which @DimmDevice is plugged in.
+ *        Default value: -1, means that slot is auto-allocated.
+ * @mr: memory region provided by host memory backend
+ */
+typedef struct DimmDevice {
+    /* private */
+    DeviceState parent_obj;
+    ram_addr_t start;
+    uint32_t node;
+    int32_t slot;
+    MemoryRegion *mr;
+} DimmDevice;
+
+typedef struct DimmDeviceClass {
+    DeviceClass parent_class;
+} DimmDeviceClass;
+
+#define TYPE_DIMM_BUS "dimm-bus"
+#define DIMM_BUS(obj) OBJECT_CHECK(DimmBus, (obj), TYPE_DIMM_BUS)
+#define DIMM_BUS_CLASS(oc) \
+    OBJECT_CLASS_CHECK(DimmBusClass, (oc), TYPE_DIMM_BUS)
+#define DIMM_BUS_GET_CLASS(obj) \
+    OBJECT_GET_CLASS(DimmBusClass, (obj), TYPE_DIMM_BUS)
+
+/**
+ * DimmBus:
+ * @parent_obj: opaque parent object container
+ */
+typedef struct DimmBus {
+    BusState parent_obj;
+} DimmBus;
+
+#endif
