e64b5c983a12687d4a87b382a28ef70dde738d36
Michael S. Tsirkin <mst@redhat.com>
2013-11-18 13:34:48 +0200

    acpi/pcihp: add minor comment
    Explain the logic behind falback for bsel == NULL.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/acpi/pcihp.c
+++ hw/acpi/pcihp.c
@@ -91,6 +91,10 @@ static PCIBus *acpi_pcihp_find_hotplug_bus(AcpiPciHpState *s, int bsel)
 
     pci_for_each_bus(s->root, acpi_pcihp_test_hotplug_bus, &find);
 
+    /* Make bsel 0 eject root bus if bsel property is not set,
+     * for compatibility with non acpi setups.
+     * TODO: really needed?
+     */
     if (!bsel && !find.bus) {
         find.bus = s->root;
     }
