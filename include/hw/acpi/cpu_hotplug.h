7bab2940eff564aa617286af7624eaba58e46eaf
Igor Mammedov <imammedo@redhat.com>
2013-12-12 04:01:05 +0100

    acpi: factor out common cpu hotplug code for PIIX4/Q35
    .. so it could be used for adding CPU hotplug to Q35 machine
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>

---
+++ include/hw/acpi/cpu_hotplug.h
@@ -0,0 +1,31 @@
+/*
+ * QEMU ACPI hotplug utilities
+ *
+ * Copyright (C) 2013 Red Hat Inc
+ *
+ * Authors:
+ *   Igor Mammedov <imammedo@redhat.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+#ifndef ACPI_HOTPLUG_H
+#define ACPI_HOTPLUG_H
+
+#include "hw/acpi/acpi.h"
+
+#define ACPI_CPU_HOTPLUG_STATUS 4
+
+#define ACPI_GPE_PROC_LEN 32
+
+typedef struct AcpiCpuHotplug {
+    MemoryRegion io;
+    uint16_t io_base;
+    uint8_t sts[ACPI_GPE_PROC_LEN];
+} AcpiCpuHotplug;
+
+void AcpiCpuHotplug_add(ACPIGPE *gpe, AcpiCpuHotplug *g, CPUState *cpu);
+
+void AcpiCpuHotplug_init(MemoryRegion *parent, Object *owner,
+                         AcpiCpuHotplug *gpe_cpu, uint16_t base);
+#endif
